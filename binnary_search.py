7451fbdd7a8532a95aa47229888559a0a40123f9
jab <evsot.vrn@gmail.com>
2023-01-26 22:04:49 +0300

    Init

---
+++ binnary_search.py
@@ -0,0 +1,20 @@
+def binary_search(list, item):
+    # this function does a binary search
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+        guess = mid
+        if guess == item:
+            return list[mid]
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 3))
